43ae8db6fca0c26b613683b0ca4682cf8a9cfa98
SuperTurk38 <d280587@edu.rocwb.nl>
2021-10-14 11:40:38 +0200

    Added Navbar and the logo(img)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,12 +2,10 @@
 
 @section('nav')
     <div class="navbar" id="home">
-        <ul>
-            <li><img class="navbar-img" src="{{asset("img/logo-wit.png")}}" alt="logo" width="60px" height="60px"></li>
-            <li><a id="sec-1" href="#home">home</a></li>
-            <li><a id="sec-2" href="#over-ons">over ons</a></li>
-            <li><a id="sec-3" href="#media">portfolio</a></li>
-            <li class="uitlijning-rechts-nav"><a id="sec-4" href="#contact">contact</a></li>
-        </ul>
+        <img class="navbar-img" src="{{asset("img/logo.png")}}" alt="logo" width="240px">
+        <a id="sec-1" href="#">Home</a>
+        <a id="sec-2" href="#">Songteksten</a>
+        <a id="sec-3" href="#">Gereedschappen</a>
+        <a id="sec-4" href="#">Contacten</a>
     </div>
 @endsection
