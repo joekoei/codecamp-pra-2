6c736e47ee05fb0e58a70f9c94d61ebfeb6c75b3
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-01 12:59:11 +0100

    Fixed .env

---
+++ app/Http/Controllers/SongtextController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SongtextController extends Controller
+{
+
+    public function bhr(){
+
+    }
+
+    public function im(){
+
+    }
+
+
+    public function kq(){
+
+    }
+
+    public function pib(){
+
+    }
+
+    public function u(){
+
+    }
+
+    public function umb(){
+
+    }
+
+}
