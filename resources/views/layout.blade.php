fec8a102cb4e0b96ce30e86a603ffad80bb9c338
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-04 09:50:38 +0100

    Fixed includes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,56 +19,9 @@
 
 </head>
 <body>
-<div class="navbar">
-    <img class="navbar-img" src="{{asset("img/logo.png")}}" alt="logo" width="240px">
-    <a href="#contact">Contact</a>
-    <div class="subnav">
-        <a href="{{route('items.book')}}">Books</a>
-    </div>
-    <div class="subnav">
-        <button class="subnavbtn">Overig <i class="fa fa-caret-down"></i></button>
-        <div class="subnav-content">
-            <a href="{{route('items.dalton')}}">Timothy Dalton</a>
-            <a href="{{route('items.dv')}}">Darth vader</a>
-            <a href="{{route('items.luke')}}">Luke Skywalker</a>
-            <a href="{{route('items.james')}}">James Bond</a>
-        </div>
-    </div>
-    <div class="subnav">
-        <button class="subnavbtn">Dranken <i class="fa fa-caret-down"></i></button>
-        <div class="subnav-content">
-            <a href="{{route('drinks.wd')}}">Warme Dranken</a>
-            <a href="{{route('drinks.f')}}">Frisdrank</a>
-        </div>
-    </div>
-    <div class="subnav">
-        <button class="subnavbtn">Gereedschappen <i class="fa fa-caret-down"></i></button>
-        <div class="subnav-content">
-            <a href="{{route('items.hamers')}}">Hamers</a>
-            <a href="{{route('items.circular')}}">Cirkelzaag</a>
-        </div>
-    </div>
-    <div class="subnav">
-        <button class="subnavbtn">Songteksten <i class="fa fa-caret-down"></i></button>
-        <div class="subnav-content">
-            <a href="{{route('song.br')}}">Bohemian Rhapsody</a>
-            <a href="{{route('song.kq')}}">Killer Queen</a>
-            <a href="{{route('song.watc')}}">We are the champions</a>
-            <a href="{{route('song.pib')}}">Plug in Baby</a>
-            <a href="{{route('song.u')}}">Uprising</a>
-            <a href="{{route('song.umb')}}">Uprising my baby</a>
-            <a href="{{route('song.mm')}}">Madness</a>
-        </div>
-    </div>
-    <a href="/">Home</a>
-</div>
-
+@include('parts.nav')
 @yield('content')
-<div class="footer" id="homepage">
-    <div class="footer-text">
-        <p>&copy; CodeCamp | Made by DevByte</p>
-    </div>
-</div>
+@include('parts.footer')
 @yield('script')
 </body>
 </html>
---
+++ resources/views/parts/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="footer" id="homepage">
+    <div class="footer-text">
+        <p>&copy; CodeCamp | Made by DevByte</p>
+    </div>
+</div>
