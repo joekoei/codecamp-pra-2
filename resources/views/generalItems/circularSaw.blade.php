1ff6039f0fe90698ff7112f74635ef43cb548a34
Tomponij <tomslavenburg@gmail.com>
2021-11-04 11:43:20 +0100

    james bond aangepast

--- resources/views/generalItems/circularSaw.blade.php
+++ resources/views/generalItems/circularSaw.blade.php
@@ -10,7 +10,7 @@
                 <p>
                 (of radiaalzaag) is een zaag met een blad in de vorm van een stalen schijf, die met een motor tot een zeer hoog toerental wordt aangedreven. De schijf kan in een zaagtafel zijn gemonteerd waarover het te zagen materiaal wordt aangevoerd, of ingebouwd in een constructie samen met een elektromotor tot draagbare handcirkelzaag. Deze heeft een minder krachtige motor en kan ook dunner materiaal zagen.
                 </p>
-                <h2>verschillendeTypen</h2>
+                <h2>verschillende Typen</h2>
                     <h3>Cirkelzaag voor non-ferromaterialen</h3>
                     <p>
                         Een cirkelzaag voor non-ferromaterialen wordt meestal gebruikt om hout mee te zagen. Op de tanden van de zaag wordt een stukje snelstaal gesoldeerd, waardoor de slijtage sterk afneemt. De belangrijkste veiligheidsvoorzieningen zijn het spouwmes en de kap.
--- resources/views/generalItems/jamesBond.blade.php
+++ resources/views/generalItems/jamesBond.blade.php
@@ -16,6 +16,11 @@
                     Live And Let Die<br>
                     For Your Eyes Only<br>
                 </p>
+                    <img id="Roger-Moore-Foto"src="{{asset("img/Roger-Moore-Foto.jpg")}}" alt="Roger Moore" width="200px height="500px">
+                <h2>Over Roger Moore</h2>
+                <p>Sir Roger George Moore (Stockwell, Lambeth, Londen, 14 oktober 1927 – Crans-Montana, Zwitserland, 23 mei 2017) was een Britse acteur die vooral bekend werd door zijn rollen in de series Ivanhoe, The Saint en The Persuaders (in Nederland uitgezonden als De Versierders en in België als De Speelvogels). Verder speelde hij de rol van geheim agent James Bond in zeven Bondfilms, die uitkwamen tussen 1973 en 1985.</p>
+                <h3>Loopbaan als James Bond</h3>
+                <p>In 1973 debuteerde hij als James Bond in Live and Let Die. De studio wilde aanvankelijk Sean Connery terug, maar die sloeg elk financieel aanbod af [1]. Roger Moore verscheen in totaal in zeven Bondfilms, één meer dan Connery. Zijn laatste optreden als Bond was in A View to a Kill (1985), hij was toen 57. In 1986 kondigde hij aan te zullen stoppen als Bond. Vergeleken met Connery en ook zijn opvolger Timothy Dalton bracht hij een humoristisch element in agent 007. Tijdens zijn Bondjaren verscheen Moore ook in andere films, zoals Gold (1974), Shout at the Devil (1976), North Sea Hijack (1979), Escape to Athena (1979) en The Sea Wolves (1980).</p>
             </div>
     </div>
 </body>
