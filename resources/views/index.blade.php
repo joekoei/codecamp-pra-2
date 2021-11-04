bc81d0668f2f06ae6c405ffde4a96de681d59676
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-04 11:49:33 +0100

    Adding maps

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,10 @@
         <div class="homePageStyle">
             <img src="{{asset("img/logo.png")}}" alt="logo">
         </div>
-            <div class="tabelHomePage">
+
+        <div id="map"></div>
+
+        <div class="tabelHomePage">
                 <table id="homePageTable">
                     <tr>
                         <th>Plaats:</th>
@@ -16,12 +19,24 @@
                         <td>Koestraat 1</td>
                     </tr>
                 </table>
+
             </div>
     </div>
 </div>
     <div class="time">
 
     </div>
+<script>
+    const uluru = { lat: 51.697363, lng: 5.293600 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 16,
+        center: uluru,
+    });
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+</script>
 @endsection
 
 <!-- @section('script')
