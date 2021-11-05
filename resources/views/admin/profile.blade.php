86323f0e65db8cf8e16a10e6a10c5e1b7d95876d
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-05 16:51:07 +0100

    Just adding dashboard standard stuff, prob dont need it in in scope but who gives a fuck

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -9,7 +9,52 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Je gegevens:
+                    <h1 class="md:text-center">Gegevens van {{Auth::user()->name}}</h1>
+                    <form action="" class="md:m-4">
+                        @csrf
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                    Je email
+                                </label>
+                            </div>
+                            <div class="md:w-2/3">
+                                <input
+                                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full
+                                     py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="inline-full-name" type="email" value="{{Auth::user()->email}}">
+                            </div>
+                        </div>
+
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                    Je naam
+                                </label>
+                            </div>
+                            <div class="md:w-2/3">
+                                <input
+                                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full
+                                     py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="inline-full-name" type="email" value="{{Auth::user()->name}}">
+                            </div>
+                        </div>
+
+                        <div class="md:flex md:items-center mb-6">
+                            <div class="md:w-1/3">
+                                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                                    Je rol
+                                </label>
+                            </div>
+                            <div class="md:w-2/3">
+                                <input readonly
+                                    class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full
+                                     py-2 px-4 text-gray-700 leading-tight focus:outline-none"
+                                    id="inline-full-name" type="email" value="{{Auth::user()->rol}}">
+                            </div>
+                        </div>
+
+                    </form>
                 </div>
             </div>
         </div>
