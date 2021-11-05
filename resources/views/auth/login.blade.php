7ed1e889a35606ee5266cb64aed2d3d56115b745
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-05 12:46:08 +0100

    Just adding some login shizzle

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,8 +43,14 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        Wachtwoord vergeten?
                     </a>
+                @endif
+
+                @if(Route::has('register'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 ml-4" href="{{ route('register') }}">
+                           Nog geen account? Registreer hier
+                        </a>
                 @endif
 
                 <x-button class="ml-3">
