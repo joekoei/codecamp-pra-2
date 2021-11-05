0b331375482fcfe75f97fdd56f7a8c44ce1c38c1
WesleyHaring <D284479@edu.rocwb.nl>
2021-11-05 11:16:25 +0100

    info warme dranken

--- resources/views/drinks/warmeDranken.blade.php
+++ resources/views/drinks/warmeDranken.blade.php
@@ -7,17 +7,17 @@
             <div class="warmeDranken">
                 <h1>Warme Dranken</h1>
                 <p>Hier komt een stuk over de drie warme dranken Koffie, Latte en Thee</p>
-                <div class="cola">
+                <div class="koffie">
                     <H3>Koffie</H3>
-                    <p></p>
+                    <p>Koffie is een meestal warm genuttigde drank, die wordt bereid op basis van water en gedroogde en gebrande pitten van de koffieplant (Coffea spp.) die vanwege hun vorm koffiebonen worden genoemd. Koffie bevat het stimulerende middel cafeïne.</p>
                 </div>
-                <div class="up">
+                <div class="latte">
                     <H3>Latte</H3>
-                    <p></p>
+                    <p>Een caffè latte of latte is een in veel Europese en Amerikaanse gelegenheden geserveerde koffiedrank, gemaakt met espresso en warme melk. Vermoedelijk ligt de oorsprong in de Verenigde Staten, waar de drank uitgevonden zou zijn om de espresso uit de opkomende koffiemachines wat te verzachten. In Italie wordt de term niet of nauwelijks gebruikt. Wie latte bestelt, krijgt melk.</p>
                 </div>
-                <div class="sinas">
+                <div class="thee">
                     <H3>Thee</H3>
-                    <p></p>
+                    <p>Thee is een warme drank die wordt gemaakt door infusie van de gedroogde bladeren van de theeplant Camellia sinensis. Alle als thee verkochte producten die niet van deze plant afkomstig zijn (bijvoorbeeld kamillethee of rooibos), zijn dan ook geen echte thee maar kruidenthee.</p>
                 </div>
             </div>
         </div>
