2ec5e6fe70f16d9af78600747f7fd7479ffae643
SuperTurk38 <d280587@edu.rocwb.nl>
2021-11-05 12:14:54 +0100

    Little fix on 'warme dranken'

--- resources/views/drinks/warmeDranken.blade.php
+++ resources/views/drinks/warmeDranken.blade.php
@@ -1,40 +1,34 @@
 @extends('layout')
 
 @section('content')
-<body class="purple">
+    <body class="purple">
     <div class="container">
         <div class="general">
             <div class="warmeDranken">
                 <h1>Warme Dranken</h1>
                 <p>Hier komt een stuk over de drie warme dranken Koffie, Latte en Thee</p>
-<<<<<<< Updated upstream
                 <div class="koffie">
                     <H3>Koffie</H3>
-                    <p>Koffie is een meestal warm genuttigde drank, die wordt bereid op basis van water en gedroogde en gebrande pitten van de koffieplant (Coffea spp.) die vanwege hun vorm koffiebonen worden genoemd. Koffie bevat het stimulerende middel cafeïne.</p>
+                    <p>Koffie is een meestal warm genuttigde drank, die wordt bereid op basis van water en gedroogde en
+                        gebrande pitten van de koffieplant (Coffea spp.) die vanwege hun vorm koffiebonen worden
+                        genoemd. Koffie bevat het stimulerende middel cafeïne.</p>
                 </div>
                 <div class="latte">
                     <H3>Latte</H3>
-                    <p>Een caffè latte of latte is een in veel Europese en Amerikaanse gelegenheden geserveerde koffiedrank, gemaakt met espresso en warme melk. Vermoedelijk ligt de oorsprong in de Verenigde Staten, waar de drank uitgevonden zou zijn om de espresso uit de opkomende koffiemachines wat te verzachten. In Italie wordt de term niet of nauwelijks gebruikt. Wie latte bestelt, krijgt melk.</p>
+                    <p>Een caffè latte of latte is een in veel Europese en Amerikaanse gelegenheden geserveerde
+                        koffiedrank, gemaakt met espresso en warme melk. Vermoedelijk ligt de oorsprong in de Verenigde
+                        Staten, waar de drank uitgevonden zou zijn om de espresso uit de opkomende koffiemachines wat te
+                        verzachten. In Italie wordt de term niet of nauwelijks gebruikt. Wie latte bestelt, krijgt
+                        melk.</p>
                 </div>
                 <div class="thee">
                     <H3>Thee</H3>
-                    <p>Thee is een warme drank die wordt gemaakt door infusie van de gedroogde bladeren van de theeplant Camellia sinensis. Alle als thee verkochte producten die niet van deze plant afkomstig zijn (bijvoorbeeld kamillethee of rooibos), zijn dan ook geen echte thee maar kruidenthee.</p>
-=======
-                <div class="cola">
-                    <H3>Koffie</H3>
-                    <p></p>
-                </div>
-                <div class="up">
-                    <H3>Latte</H3>
-                    <p></p>
-                </div>
-                <div class="sinas">
-                    <H3>Thee</H3>
-                    <p></p>
->>>>>>> Stashed changes
+                    <p>Thee is een warme drank die wordt gemaakt door infusie van de gedroogde bladeren van de theeplant
+                        Camellia sinensis. Alle als thee verkochte producten die niet van deze plant afkomstig zijn
+                        (bijvoorbeeld kamillethee of rooibos), zijn dan ook geen echte thee maar kruidenthee.</p>
                 </div>
             </div>
         </div>
     </div>
-</body>
+    </body>
 @endsection
