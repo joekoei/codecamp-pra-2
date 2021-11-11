99e42302c3bb4ac2ed9d2edfd6d9344c3f0e5712
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-11 11:47:23 +0100

    Fixed small bug

--- resources/views/admin/edit/book.blade.php
+++ resources/views/admin/edit/book.blade.php
@@ -41,7 +41,7 @@
                             </div>
                         </div>
 
-                        @if(Auth::user->rol === 'sales')
+                        @if(Auth::user()->rol === 'sales')
                             <div class="md:flex md:items-center mb-6">
                                 <div class="md:w-1/3">
                                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
