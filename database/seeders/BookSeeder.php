75e0d9ef78754dd99251f63ad977d9ad5254218d
Nusret <nusret2004@hotmail.nl>
2021-11-11 14:33:50 +0100

    Added 3 books to complete the 10

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -16,51 +16,72 @@ class BookSeeder extends Seeder
     {
         $book = new Book();
         $book->name = 'Harry Potter en de steen de Wijzen';
-        $book->price = '25.0';
+        $book->price = '12,50';
         $book->auteur = 'JK Rowling';
-        $book->pages = '120';
+        $book->pages = '258';
         $book->save();
 
         $book = new Book();
         $book->name = 'Hitchhikers guide tot he Universe';
-        $book->price = '0';
+        $book->price = '19,90';
         $book->auteur = 'Douglas Adams';
-        $book->pages = '120';
+        $book->pages = '336';
         $book->save();
 
         $book = new Book();
         $book->name = 'Oorlog en Vrede';
-        $book->price = '0';
+        $book->price = '37,50';
         $book->auteur = 'Tolsjoy';
-        $book->pages = '120';
+        $book->pages = '1624';
         $book->save();
 
         $book = new Book();
         $book->name = 'Moonraker';
-        $book->price = '0';
+        $book->price = '14,99';
         $book->auteur = 'Ian Fleming';
-        $book->pages = '120';
+        $book->pages = '245';
         $book->save();
 
         $book = new Book();
         $book->name = 'Dr No';
-        $book->price = '0';
+        $book->price = '15,00';
         $book->auteur = 'Ian Fleming';
-        $book->pages = '120';
+        $book->pages = '286';
         $book->save();
 
         $book = new Book();
         $book->name = 'Turks Fruit';
-        $book->price = '0';
+        $book->price = '9,99';
         $book->auteur = 'Jan Wolkers';
-        $book->pages = '120';
+        $book->pages = '192';
         $book->save();
 
         $book = new Book();
         $book->name = 'Wurgtechnieken';
-        $book->price = '0';
-        $book->auteur = 'Ward Ruislink';
+        $book->price = '13.99';
+        $book->auteur = 'Ward Ruyslink';
+        $book->pages = '205';
+        $book->save();
+
+        $book = new Book();
+        $book->name =  'The Lord of the Rings';
+        $book->price = '29,99';
+        $book->auteur = 'J.R.R. Tolkien';
+        $book->pages = '480';
+        $book->save();
+
+        $book = new Book();
+        $book->name =  'The Little Prince';
+        $book->price = '9,95';
+        $book->auteur = 'Antoine de Saint-Exupéry';
         $book->pages = '120';
+        $book->save();
+
+        $book = new Book();
+        $book->name =  'Charlie and the Chocolate Factory';
+        $book->price = '7,99';
+        $book->auteur = 'Roald Dahl';
+        $book->pages = '192';
         $book->save();
     }
 }
