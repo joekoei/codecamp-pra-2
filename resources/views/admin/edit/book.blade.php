0efe911a50c51648a5b55d871c5b73cde309b068
Joey Blommers <joeyblommers2004@gmail.com>
2021-11-12 11:35:38 +0100

    Added all beheerder rol permissions

--- resources/views/admin/edit/book.blade.php
+++ resources/views/admin/edit/book.blade.php
@@ -41,7 +41,7 @@
                             </div>
                         </div>
 
-                        @if(Auth::user()->rol === 'sales')
+                        @if(Auth::user()->rol === 'sales' || Auth::user()->rol === 'beheerder')
                             <div class="md:flex md:items-center mb-6">
                                 <div class="md:w-1/3">
                                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
@@ -57,7 +57,7 @@
                             </div>
                         @endif
 
-                        @if(Auth::user()->rol === 'content')
+                        @if(Auth::user()->rol === 'content' || Auth::user()->rol === 'beheerder')
                             <div class="md:flex md:items-center mb-6">
                                 <div class="md:w-1/3">
                                     <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
